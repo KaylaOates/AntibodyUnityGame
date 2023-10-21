7f94a4bdd9a0414be0eebf443e31465ffca08456
Natalia Rojas <natalia.rojas@ufl.edu>
2023-10-21 15:44:04 -0600

    Update TutorialManager.cs

--- UnityGame/Assets/Scripts/TutorialManager.cs
+++ UnityGame/Assets/Scripts/TutorialManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject[] popUps;
+    private int popUpIndex;
+
     void Start()
     {
-        
+        player.jumpForce = 0;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        
+        for (int i = 0; i < popUps.Length; i++)
+        {
+            if(i == popUpIndex) { 
+                popUps[i].SetActive(true);
+            } else
+            {
+                popUps[i].SetActive(false);
+            }
+        }
+        if(popUpIndex == 0)
+        {
+            if(Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                popUpIndex++;
+            }
+        } else if(popUpIndex == 1)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                player.jumpForce = 8;
+                popUpIndex++;
+            }
+
+        }
     }
 }
