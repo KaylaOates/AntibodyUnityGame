bc8776f066b2055cf7bf254c0551f3af13426a01
KaylaOates <oatesk@ufl.edu>
2023-10-21 17:28:43 -0400

    hopefully fixed the scaling

--- UnityGame/Assets/Scripts/AntibodyController.cs
+++ UnityGame/Assets/Scripts/AntibodyController.cs
@@ -10,31 +10,48 @@ public class AntibodyController : MonoBehaviour
     float vertical, horizontal;
     Rigidbody2D myRigidbody;
 
-    /*float minX = 200;
+    float minX;
     float maxX;
-    float minY = 200;
-    float maxY;*/
+    float minY;
+    float maxY;
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        //maxX = Screen.width - 200;
-        //maxY = Screen.height - 200;
+        minX = - (Screen.width/2 - 200) ;
+        maxX = (Screen.width/2 - 200);
+        minY = - (Screen.height/2 - 200);
+        maxY = (Screen.height/2 - 200);
+
+        Debug.Log("minX: " + minX);
+        Debug.Log("maxX: " + maxX);
+        Debug.Log("minY: " + minY);
+        Debug.Log("maxY: " + maxY);
+
     }
 
     private void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        
         myRigidbody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
+        
+        Vector2 position = myRigidbody.position;
+        float clampedX = Mathf.Clamp(position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(position.y, minY, maxY);
+        if (position.x < minX || position.x > maxX || position.y < minY || position.y > maxY)
+        {
+            Debug.Log("Out of bounds: " + position.x + ", " + position.y);
+            myRigidbody.position = new Vector2(clampedX, clampedY);
+        }
 
-        //Vector2 position = myRigidbody.position;
-
-        //float clampedX = Mathf.Clamp(position.x, minX, maxX);
-        //float clampedY = Mathf.Clamp(position.y, minY, maxY);
+        /*Vector2 position = myRigidbody.position;
+        float clampedX = Mathf.Clamp(position.x, minX, maxX);
+        float clampedY = Mathf.Clamp(position.y, minY, maxY);
 
         // Check if the position is within the bounds
-        /*if (position.x >= minX && position.x <= maxX && position.y >= minY && position.y <= maxY)
+        if (position.x >= minX && position.x <= maxX && position.y >= minY && position.y <= maxY)
         {
             // Rigidbody is within bounds, allow movement
             myRigidbody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
