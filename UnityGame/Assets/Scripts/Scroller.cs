c7e6c0d68010120c61a8397f7e91250c5aee3553
Natalia Rojas <natalia.rojas@ufl.edu>
2023-10-21 15:39:46 -0600

    Update Scroller.cs

--- UnityGame/Assets/Scripts/Scroller.cs
+++ UnityGame/Assets/Scripts/Scroller.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 
 public class Scroller : MonoBehaviour
 {
-    [SerializeField] private RawImage _img;
-    [SerializeField] private float _x, _y;
+    //[SerializeField] private RawImage _img;
+    //[SerializeField] private float _x, _y;
 
 
     // Update is called once per frame
-    void Update()
-    {
-        _img.uvRect = new Rect(_img.uvRect.position + new Vector(_x, _y) * Time.deltaTime, _img.uvRect.size);
-    }
+    //void Update()
+  //  {
+    //    _img.uvRect = new Rect(_img.uvRect.position + new Vector(_x, _y) * Time.deltaTime, _img.uvRect.size);
+    //}
 }
