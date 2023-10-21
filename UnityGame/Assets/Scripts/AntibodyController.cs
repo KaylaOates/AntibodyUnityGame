364d620328410a8698c0d7a2486741c047dc7811
KaylaOates <oatesk@ufl.edu>
2023-10-21 16:52:52 -0400

    removed clamp on boarders

--- UnityGame/Assets/Scripts/AntibodyController.cs
+++ UnityGame/Assets/Scripts/AntibodyController.cs
@@ -10,30 +10,31 @@ public class AntibodyController : MonoBehaviour
     float vertical, horizontal;
     Rigidbody2D myRigidbody;
 
-    float minX = 200;
+    /*float minX = 200;
     float maxX;
     float minY = 200;
-    float maxY;
+    float maxY;*/
 
     private void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
-        maxX = Screen.width - 200;
-        maxY = Screen.height - 200;
+        //maxX = Screen.width - 200;
+        //maxY = Screen.height - 200;
     }
 
     private void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
         vertical = Input.GetAxisRaw("Vertical");
+        myRigidbody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
 
-        Vector2 position = myRigidbody.position;
+        //Vector2 position = myRigidbody.position;
 
-        float clampedX = Mathf.Clamp(position.x, minX, maxX);
-        float clampedY = Mathf.Clamp(position.y, minY, maxY);
+        //float clampedX = Mathf.Clamp(position.x, minX, maxX);
+        //float clampedY = Mathf.Clamp(position.y, minY, maxY);
 
         // Check if the position is within the bounds
-        if (position.x >= minX && position.x <= maxX && position.y >= minY && position.y <= maxY)
+        /*if (position.x >= minX && position.x <= maxX && position.y >= minY && position.y <= maxY)
         {
             // Rigidbody is within bounds, allow movement
             myRigidbody.velocity = new Vector2(horizontal * moveSpeed, vertical * moveSpeed);
@@ -43,7 +44,6 @@ public class AntibodyController : MonoBehaviour
             // Rigidbody is out of bounds, clamp its position
             myRigidbody.position = new Vector2(clampedX, clampedY);
             myRigidbody.velocity = Vector2.zero;
-        }
-
+        }*/
     }
 }
