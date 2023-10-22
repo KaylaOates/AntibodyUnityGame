41f4f7ea038eca408e6b5faadad49bfff48de100
danggtifff <63815352+danggtifff@users.noreply.github.com>
2023-10-21 20:54:17 -0400

    Finished spawning
    increasing spawn speed over time and inplemented 2 more virus prefabs and added that to the spawning script

--- UnityGame/Assets/Scripts/Spawner.cs
+++ UnityGame/Assets/Scripts/Spawner.cs
@@ -9,24 +9,38 @@ public class Spawner : MonoBehaviour
     private GameObject red_blood_cellPrefab;
     [SerializeField]
     private GameObject virus_greenPrefab;
+    [SerializeField]
+    private GameObject virus_bluePrefab;
+    [SerializeField]
+    private GameObject virus_yellowPrefab;
 
     [SerializeField]
     private float red_blood_cellInterval = 3.5f;
     [SerializeField]
-    private float virus_greenInterval = 10f; 
+    private float virus_greenInterval = 3f;
+    [SerializeField]
+    private float virus_yellowInterval = 3f;
+    [SerializeField]
+    private float virus_blueInterval = 3f;
 
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(spawnEnemy(red_blood_cellInterval, red_blood_cellPrefab));
         StartCoroutine(spawnEnemy(virus_greenInterval, virus_greenPrefab));
+        StartCoroutine(spawnEnemy(virus_blueInterval, virus_bluePrefab));
+        StartCoroutine(spawnEnemy(virus_yellowInterval, virus_yellowPrefab));
     }
 
     // Update is called once per frame
     private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
+
         yield return new WaitForSeconds(interval);
-        GameObject newEnemy = Instantiate(enemy, new Vector3(10f, Random.Range(-4.7f, 4.7f), 0), Quaternion.identity);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(10f, Random.Range(-4.5f, 4.5f), 0), Quaternion.identity);
         StartCoroutine(spawnEnemy(interval, enemy));
+        virus_greenInterval += 4f * Time.deltaTime;
+        virus_yellowInterval += 4f * Time.deltaTime;
+        virus_blueInterval += 4f * Time.deltaTime;
     }
 }
